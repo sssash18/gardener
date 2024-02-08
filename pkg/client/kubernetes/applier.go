af2eaa34415dde0bc5d343ce091b21f2cd5cedfc
Suyash Choudhary <57896905+sssash18@users.noreply.github.com>
2024-02-08 10:47:05 +0530

    Experiment

--- pkg/client/kubernetes/applier.go
+++ pkg/client/kubernetes/applier.go
@@ -99,10 +99,7 @@ func (a *defaultApplier) applyObject(ctx context.Context, desired *unstructured.
 		}
 		return err
 	}
-	fmt.Println("????????????")
-	fmt.Println(current)
-	fmt.Println("????????????")
-	fmt.Println(desired)
+
 	if err := a.mergeObjects(desired, current, options); err != nil {
 		return err
 	}
